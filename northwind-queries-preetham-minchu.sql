4441f5b553f9d863722e7b98f15247284aad7b11
PreettamM03 <preetham.madesh03@gmail.com>
2023-07-11 20:21:21 -0400

    Final Project Commit

---
+++ northwind-queries-preetham-minchu.sql
@@ -0,0 +1,36 @@
+use northwind;
+
+/*What are the categories of products in the database?
+SELECT category FROM products;
+
+/*What products are made by Dell?
+SELECT * FROM products
+WHERE product_name LIKE '%Dell%'
+
+/*List all the orders shipped to Pennsylvania.*/
+SELECT * FROM orders 
+WHERE ship_state='Pennsylvania';
+
+/*List the first name and last name of all employees with last names that start with the letter W.*/
+SELECT * FROM employees
+WHERE last_name LIKE 'a%';
+
+/*List all customers from zip codes that start with 55.*/
+SELECT * FROM customers
+WHERE postal_code like '55%';
+
+/*List all customers from zip codes that end with 0.*/
+SELECT * FROM customers
+WHERE postal_code like '%0';
+/*List the first name, last name, and email for all customers with a ".org" email address.*/
+SELECT first_name, last_name, email from customers 
+WHERE email like '%.org';
+
+/*List the first name, last name, and phone number for all customers from the 202 area code.*/
+SELECT first_name, last_name, phone from customers 
+WHERE phone like '1-(202)%';
+
+/*List the first name, last name, and phone number for all customers from the 202 area code, ordered by last name, first name.*/
+SELECT first_name, last_name, phone from customers 
+WHERE phone like '1-(202)%'
+ORDER BY last_name, first_name
